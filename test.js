8254bdd963c9a04accf474a061675ca61a08419e
Kevin Martensson <kevinmartensson@gmail.com>
2016-08-24 11:34:33 +0200

    Add `stdin` test

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import test from 'ava';
 import execa from 'execa';
 import fileType from 'file-type';
+import pify from 'pify';
 
 process.chdir(__dirname);
 
@@ -17,6 +19,18 @@ test('convert images to ico', async t => {
 		'fixtures/16.png',
 		'fixtures/32.png'
 	], {encoding: 'buffer'})), {
+		ext: 'ico',
+		mime: 'image/x-icon'
+	});
+});
+
+test('convert image to ico using stdin', async t => {
+	const input = await pify(fs.readFile)('fixtures/16.png');
+
+	t.deepEqual(fileType(await execa.stdout('./cli.js', {
+		input,
+		encoding: 'buffer'
+	})), {
 		ext: 'ico',
 		mime: 'image/x-icon'
 	});
